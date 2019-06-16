b7d2fe321239e6873cb866a3f1afe095c89a44a4
0XCAFEBABE <283773833@qq.com>
2019-06-16 20:32:02 +0800

    Ribbon消费者

---
+++ hello-spring-cloud-web-admin-ribbon/src/main/java/com/funtl/hello/spring/cloud/web/admin/ribbon/WebAdminRibbonApplication.java
@@ -0,0 +1,14 @@
+package com.funtl.hello.spring.cloud.web.admin.ribbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+//发现服务
+@EnableDiscoveryClient
+@SpringBootApplication
+public class WebAdminRibbonApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebAdminRibbonApplication.class, args);
+    }
+}
