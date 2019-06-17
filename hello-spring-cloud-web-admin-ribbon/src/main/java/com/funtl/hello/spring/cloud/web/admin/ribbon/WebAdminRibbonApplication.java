7cf8244633c3c5f2694aebaff13b617bc2caee2c
0XCAFEBABE <283773833@qq.com>
2019-06-17 20:49:08 +0800

    Ribbon熔断器

--- hello-spring-cloud-web-admin-ribbon/src/main/java/com/funtl/hello/spring/cloud/web/admin/ribbon/WebAdminRibbonApplication.java
+++ hello-spring-cloud-web-admin-ribbon/src/main/java/com/funtl/hello/spring/cloud/web/admin/ribbon/WebAdminRibbonApplication.java
@@ -3,7 +3,10 @@ package com.funtl.hello.spring.cloud.web.admin.ribbon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
+//Ribbon熔断器
+@EnableHystrix
 //发现服务
 @EnableDiscoveryClient
 @SpringBootApplication
