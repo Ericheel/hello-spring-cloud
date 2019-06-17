6a09c7c1ce338c61577df044e6dbff839cf7e9a0
0XCAFEBABE <283773833@qq.com>
2019-06-17 22:32:25 +0800

    Feign熔断器

--- hello-spring-cloud-web-admin-feign/src/main/java/com/funtl/hello/spring/cloud/web/admin/feign/service/AdminService.java
+++ hello-spring-cloud-web-admin-feign/src/main/java/com/funtl/hello/spring/cloud/web/admin/feign/service/AdminService.java
@@ -1,11 +1,12 @@
 package com.funtl.hello.spring.cloud.web.admin.feign.service;
 
+import com.funtl.hello.spring.cloud.web.admin.feign.service.hystrix.AdminServiceHystrix;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 //服务提供者
-@FeignClient(value = "hello-spring-cloud-service-admin")
+@FeignClient(value = "hello-spring-cloud-service-admin", fallback = AdminServiceHystrix.class)
 public interface AdminService {
 
     @GetMapping("/hi")
